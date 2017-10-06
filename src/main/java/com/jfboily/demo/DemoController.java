93120bca616e07737ce1a251840961a32479874d
jf boily <jf@jfboily.com>
2017-10-05 21:18:17 -0400

    Starter project

---
+++ src/main/java/com/jfboily/demo/DemoController.java
@@ -0,0 +1,14 @@
+package com.jfboily.demo;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello world!";
+    }
+}
